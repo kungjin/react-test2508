088326f1ef38ee7ea774bda6694cf7cca62f7ea1
kungjin <vudchsdl123@gmail.com>
2025-08-20 16:31:25 +0900

    Updates

--- src/components/Banner.jsx
+++
@@ -1,9 +0,0 @@
-export default function Banner() {
-  return (
-    <aside className="card" aria-label="프로모션 배너">
-      <a className="banner" href="#">
-        <img src="https://picsum.photos/seed/banner/600/180" alt="프로모션 배너" />
-      </a>
-    </aside>
-  );
-}
